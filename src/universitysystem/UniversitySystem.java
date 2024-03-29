d72e543ca593862e84ad09766f1c24471795f532
Mark John <markjohnjopia1@gmail.com>
2024-03-29 14:27:02 +0800

    First Commit

---
+++ src/universitysystem/UniversitySystem.java
@@ -0,0 +1,11 @@
+package universitysystem2;
+
+public class UniversitySystem2 {
+    
+    public static void main(String[] args) {
+         Menu menu = new Menu();
+         menu.setVisible(true);
+       
+    }
+    
+}
